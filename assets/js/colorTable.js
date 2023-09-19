8eb0cf4b5a5c2b619fa90902350a492e34eddf8b
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2023-09-19 17:12:35 +0700

    up

--- assets/js/colorTable.js
+++ assets/js/colorTable.js
@@ -1,24 +1,33 @@
 // color of temperature max
 const temperatureMax = document.querySelector('.max');
-
 temperatureMax.setAttribute("style","background-color: #E4AD2B");
+
+
 
 // color of temperature min
 const temperatureMin = document.querySelector('.min');
-
 temperatureMin.setAttribute("style","background-color: #E9D97E");
+
+
 
 // color of avg
 const avg = document.querySelector('.avg');
-
 avg.setAttribute("style","background-color: #F1C151");
+
+
 
 // precip
 const precip = document.querySelector('.precip');
-
 precip.setAttribute("style","background-color: #0899CA");
+
+
 
 // max wind
 const maxWind = document.querySelector('.max-wind');
-
 maxWind.setAttribute("style","background-color: #A4F6E9");
+
+
+
+// color subtable
+let arr = [80 , 90 , 75 , 70];
+
